9ca160a7b6d3d8cec2529f7ae0a220b7062af336
seo2730 <42115807+seo2730@users.noreply.github.com>
2020-08-22 15:07:20 +0900

    Update EulerToQuaternion.m

--- 4. Kalman_Filter/6. Horizontal position/position/EulerToQuaternion.m
+++ 4. Kalman_Filter/6. Horizontal position/position/EulerToQuaternion.m
@@ -7,6 +7,6 @@ sinPsi   = sin(psi/2);   cosPsi   = cos(psi/2);
 z = [cosPhi*cosTheta*cosPsi + sinPhi*sinTheta*sinPsi;
      sinPhi*cosTheta*cosPsi - cosPhi*sinTheta*sinPsi;
      cosPhi*sinTheta*cosPsi + sinPhi*cosTheta*sinPsi;
-     cosPhi*cosTheta*sinPsi + sinPhi*sinTheta*cosPsi];
+     cosPhi*cosTheta*sinPsi - sinPhi*sinTheta*cosPsi];
  
 end
